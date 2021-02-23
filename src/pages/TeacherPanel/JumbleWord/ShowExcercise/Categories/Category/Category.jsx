72eddc042fa471f33c5e9a9c94b841c6a6465192
SheKing <oofricoo@gmail.com>
2021-02-23 16:00:14 +0100

    Change folder structure in JumbleWord

--- src/pages/TeacherPanel/JumbleWord/ShowExcercise/Categories/Category/Category.jsx
+++ src/pages/TeacherPanel/JumbleWord/ShowExcercise/Categories/Category/Category.jsx
@@ -14,7 +14,7 @@ import {
 } from '@material-ui/core';
 import { ExpandLess, ExpandMore } from '@material-ui/icons';
 
-import Word from '../Word/Word';
+import Word from './Word/Word';
 
 const useStyles = makeStyles((theme) => ({
   categoryName: {
