32bb7089a219905be1ef43dc744c2c7083981965
SheKing <oofricoo@gmail.com>
2021-02-24 17:21:49 +0100

    Apply ExcerciseGenerator template to JumbleWord

--- src/pages/TeacherPanel/JumbleWord/JumbleWord.jsx
+++ src/pages/TeacherPanel/JumbleWord/JumbleWord.jsx
@@ -1,19 +1,13 @@
 import React, { useEffect } from 'react';
-import { Provider, useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
-import { Helmet } from 'react-helmet';
-
-import { Divider, Grid, makeStyles, Paper } from '@material-ui/core';
-
-import store from '../../../store/store';
 import {
   setData,
   setExcercise,
   setExcerciseEmptyCategory,
-  setSnackbar,
 } from '../../../store/reducers/jumbleWordSlice';
 
-import UniversalSnackbarAlert from '../../../components/universal/UniversalSnackbarAlert/UniversalSnackbarAlert';
+import ExcerciseGenerator from '../../../components/ExcerciseGenerator/ExcerciseGenerator';
 import Header from './subcomponents/Header/Header';
 import ShowExcercise from './subcomponents/ShowExcercise/ShowExcercise';
 import AddWord from './subcomponents/AddWord/AddWord';
@@ -43,37 +37,12 @@ const getData = async () => {
   return data;
 };
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    marginTop: theme.spacing(3),
-  },
-  paper: {
-    padding: theme.spacing(2),
-  },
-}));
-
 const JumbleWord = () => {
   const addExcerciseCount = useSelector(
     (state) => state.jumbleWord.addExcerciseCount
   );
-  const snackbarData = useSelector((state) => state.jumbleWord.snackbarData);
 
   const dispatch = useDispatch();
-
-  const classes = useStyles();
-
-  const handleSnackbarClose = (event, reason) => {
-    if (reason === 'clickaway') {
-      return;
-    }
-
-    dispatch(
-      setSnackbar({
-        open: false,
-      })
-    );
-  };
 
   // data fetch
   useEffect(() => {
@@ -94,36 +63,12 @@ const JumbleWord = () => {
   }, [addExcerciseCount, dispatch]);
 
   return (
-    <div className={classes.root}>
-      <Helmet>
-        <title>ENGLISHTREE - Random word</title>
-      </Helmet>
-
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <Header />
-        </Grid>
-
-        <Grid item xs={12}>
-          <Divider />
-        </Grid>
-
-        {/* ADDING DATA */}
-        <Grid component="section" item lg={6} md={6} sm={12} xs={12}>
-          <Paper className={classes.paper}>
-            <AddWord />
-          </Paper>
-        </Grid>
-
-        {/* SHOWING DATA */}
-        <Grid component="section" item lg={6} md={6} sm={12} xs={12}>
-          <Paper className={classes.paper}>
-            <ShowExcercise />
-          </Paper>
-        </Grid>
-      </Grid>
-      <UniversalSnackbarAlert handleClose={handleSnackbarClose} {...snackbarData} />
-    </div>
+    <ExcerciseGenerator
+      title="Random word"
+      header={<Header />}
+      addData={<AddWord />}
+      displayData={<ShowExcercise />}
+    />
   );
 };
 
