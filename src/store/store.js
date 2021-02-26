824f06e225fba779af973b2a5358ecf68d72a13e
SheKing <oofricoo@gmail.com>
2021-02-26 19:47:35 +0100

    Feat: Add local storage for holding app state

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+import { throttle } from 'lodash';
+
+import { loadState, saveState } from './localStorage';
+
 import jumbleSentenceReducer from './reducers/jumbleSentenceSlice';
 import jumbleWordReducer from './reducers/jumbleWordSlice';
 import snackbarReducer from './reducers/snackbarSlice';
 import themeSliceReducer from './reducers/themeSlice';
 
-export default configureStore({
+const persistedState = loadState();
+
+const store = configureStore({
+  preloadedState: persistedState,
   reducer: {
     jumbleSentence: jumbleSentenceReducer,
     jumbleWord: jumbleWordReducer,
@@ -13,3 +20,13 @@ export default configureStore({
     theme: themeSliceReducer,
   },
 });
+
+store.subscribe(
+  throttle(() => {
+    saveState({
+      theme: store.getState().theme,
+    });
+  }, 1000)
+);
+
+export default store;
